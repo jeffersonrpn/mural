d0f27b69bc08a3bc9f93964fc3a294529b6a6b31
Jefferson Neves <jefferson.rpn@gmail.com>
2016-10-22 18:06:57 -0300

    Change angular-route to ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,36 +1,43 @@
-'use strict';
+(function() {
+  'use strict';
 
-/**
- * @ngdoc overview
- * @name muralApp
- * @description
- * # muralApp
- *
- * Main module of the application.
- */
-angular
-  .module('muralApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  /**
+   * @ngdoc overview
+   * @name muralApp
+   * @description
+   * # muralApp
+   *
+   * Main module of the application.
+   */
+  angular
+    .module('muralApp', [
+      'ngAnimate',
+      'ngAria',
+      'ngCookies',
+      'ngResource',
+      'ngSanitize',
+      'ngTouch',
+      'ui.router'
+    ])
+    .config(routeConfig);
+
+    routeConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    /*jshint latedef: nofunc */
+    function routeConfig($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('main', {
+          url: "/",
+          templateUrl: "views/main.html",
+          controller: "MainCtrl",
+          controllerAs: "ctrl"
+        })
+        .state('about', {
+          url: "/about",
+          templateUrl: "views/about.html",
+          controller: "AboutCtrl",
+          controllerAs: "ctrl"
+        });
+      $urlRouterProvider.otherwise('/');
+    }
+})();
