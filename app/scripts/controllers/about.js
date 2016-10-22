4249299eab1bacf8defbc1a5072b7b712c57a2d5
Jefferson Neves <jefferson.rpn@gmail.com>
2016-10-22 16:59:09 -0300

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name muralApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the muralApp
+ */
+angular.module('muralApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name muralApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the muralApp
+ */
+angular.module('muralApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
