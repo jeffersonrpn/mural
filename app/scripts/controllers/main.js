c5c743ea4ace6852c3cc1708c089f90d88e4781d
Jefferson Neves <jefferson.rpn@gmail.com>
2016-10-25 07:52:15 -0300

    WIP

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,8 +16,22 @@
   /*jshint latedef: nofunc */
   function MainCtrl(Posts, Auth, $state) {
     var vm = this;
+    vm.posts = [];
+    vm.getComments = getComments;
 
-    vm.posts = Posts.query();
+    function init() {
+      Posts.query(function(response) {
+        vm.posts = response;
+        vm.posts.comments = [];
+      });
+    }
+    init();
+
+    function getComments(post) {
+      Posts.comments({id: post.id}, function(response) {
+        // post
+      });
+    }
 
   }
 })();
