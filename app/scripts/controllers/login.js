44fb59565d92854a2f896557340425822e1fcd4d
Jefferson Neves <jefferson.rpn@gmail.com>
2016-10-25 21:21:05 -0300

    Update login page style

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -11,20 +11,27 @@
   angular.module('muralApp')
     .controller('LoginCtrl', LoginCtrl);
 
-  LoginCtrl.$inject = ['$state', 'Auth'];
+  LoginCtrl.$inject = ['$state', '$timeout', 'Auth'];
 
   /*jshint latedef: nofunc */
-  function LoginCtrl($state, Auth) {
+  function LoginCtrl($state, $timeout, Auth) {
     var vm = this;
-
     vm.username = '';
+    vm.formError = false;
+    vm.isLoading = false;
     vm.doLogin = doLogin;
 
     function doLogin() {
+      vm.formError = false;
+      vm.isLoading = true;
       Auth.authenticate(vm.username, function() {
         $state.go('main');
       }, function() {
         console.log("Auth error");
+        vm.isLoading = false;
+        $timeout(function() {
+          vm.formError = true;
+        }, 500);
       });
     }
 
